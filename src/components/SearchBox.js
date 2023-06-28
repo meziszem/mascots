c0c49f02a1460c694a72741965e13a8eef7722e0
meziszem <meseretfh@gmail.com>
2023-06-28 11:30:54 -0400

    adjusted searchbox border radius

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({searchChange}) => {
     return (
         <div className='pa3'>
         <input
-        className='pa3 ba b--dark-blue bg-lightest-blue' 
+        className='pa3 br3 ba b--dark-blue bg-lightest-blue' 
         type='search' 
         placeholder='Search Mascots' 
         onChange = {searchChange}
